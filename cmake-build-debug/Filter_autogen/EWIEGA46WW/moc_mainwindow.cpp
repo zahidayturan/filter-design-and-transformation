d6e568ea38f1c3901721f7f195829ce9a949f0f7
zahidayturan <21060607@stu.omu.edu.tr>
2024-11-25 22:40:35 +0300

    autogen rec

--- cmake-build-debug/Filter_autogen/EWIEGA46WW/moc_mainwindow.cpp
+++ cmake-build-debug/Filter_autogen/EWIEGA46WW/moc_mainwindow.cpp
@@ -37,9 +37,10 @@ namespace {
 struct qt_meta_stringdata_CLASSMainWindowENDCLASS_t {};
 constexpr auto qt_meta_stringdata_CLASSMainWindowENDCLASS = QtMocHelpers::stringData(
     "MainWindow",
-    "generateCSV",
+    "generateButterworthCSV",
     "",
-    "showGraph",
+    "generateChebyshevCSV",
+    "generateInverseChebyshevCSV",
     "showGraphWithPath",
     "filename"
 );
@@ -54,7 +55,7 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
       12,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       4,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -62,14 +63,16 @@ Q_CONSTINIT static const uint qt_meta_data_CLASSMainWindowENDCLASS[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags, initial metatype offsets
-       1,    0,   32,    2, 0x08,    1 /* Private */,
-       3,    0,   33,    2, 0x08,    2 /* Private */,
-       4,    1,   34,    2, 0x08,    3 /* Private */,
+       1,    0,   38,    2, 0x08,    1 /* Private */,
+       3,    0,   39,    2, 0x08,    2 /* Private */,
+       4,    0,   40,    2, 0x08,    3 /* Private */,
+       5,    1,   41,    2, 0x08,    4 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::QString,    5,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    6,
 
        0        // eod
 };
@@ -83,9 +86,11 @@ Q_CONSTINIT const QMetaObject MainWindow::staticMetaObject = { {
     qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSMainWindowENDCLASS_t,
         // Q_OBJECT / Q_GADGET
         QtPrivate::TypeAndForceComplete<MainWindow, std::true_type>,
-        // method 'generateCSV'
+        // method 'generateButterworthCSV'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
-        // method 'showGraph'
+        // method 'generateChebyshevCSV'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'generateInverseChebyshevCSV'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
         // method 'showGraphWithPath'
         QtPrivate::TypeAndForceComplete<void, std::false_type>,
@@ -100,9 +105,10 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         auto *_t = static_cast<MainWindow *>(_o);
         (void)_t;
         switch (_id) {
-        case 0: _t->generateCSV(); break;
-        case 1: _t->showGraph(); break;
-        case 2: _t->showGraphWithPath((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
+        case 0: _t->generateButterworthCSV(); break;
+        case 1: _t->generateChebyshevCSV(); break;
+        case 2: _t->generateInverseChebyshevCSV(); break;
+        case 3: _t->showGraphWithPath((*reinterpret_cast< std::add_pointer_t<QString>>(_a[1]))); break;
         default: ;
         }
     }
@@ -127,13 +133,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 4)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 4;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 4)
             *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
-        _id -= 3;
+        _id -= 4;
     }
     return _id;
 }
